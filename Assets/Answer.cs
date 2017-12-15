c197abeec2c6d7509dfb638d8ac6db23eac96ae2
HaRu <vharugun9093@gmail.com>
2017-12-16 04:16:33 +0900

    Repair

---
+++ Assets/Answer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    public GameObject First;
+    public GameObject Second;
+    public GameObject Third;
+}
