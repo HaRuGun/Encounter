dcb981438382122774b3cfe6039ccb65cec1c25c
koreacoderider <koreacoderider@gmail.com>
2017-12-16 20:36:34 +0900

    SceneManager, SoundManager, FinalScene
    SceneManager, SoundManager, FinalScene

--- Assets/Scripts/SceneManagerScr.cs
+++ Assets/Scripts/SceneManagerScr.cs
@@ -1,11 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerScr : MonoBehaviour {
 
-    public void SceneChange1() {
+    public Image splashImage;
+    public string loadLevel;
+
+    int rightCount;
+    int wrongCount;
+    float friendship;
+
+    public static SceneManagerScr instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        Start();
+    }
+
+    IEnumerator Start() {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    public void FadeIn() {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false); 
+    }
+
+    public void FadeOut(){
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+
+    public void SceneChange1()
+    {
+        Debug.Log("1");
         SceneManager.LoadScene(1);
     }
+
+    public void SceneChange(string sceneName) {
+        Debug.Log("2");
+        SceneManager.LoadScene(sceneName);
+    }
+
 }
---
+++ Assets/SoundManagerScr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScr : MonoBehaviour {
+
+    public static SoundManagerScr instance = null;
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        //임의의 함수
+        
+    }
+
+    // Update is called once per frame
+    void MusicPlaySingle(AudioClip clip) {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    void MusicStopSingle()
+    {
+        efxSource.Stop();
+    }
+
+    // Update is called once per frame
+    void EfxPlaySingle(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    void EfxStopSingle()
+    {
+        musicSource.Stop();
+    }
+}
