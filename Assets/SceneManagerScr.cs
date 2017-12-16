b1075244c453eb3c752c69b369e77a3b444e5122
koreacoderider <koreacoderider@gmail.com>
2017-12-16 12:40:00 +0900

    SceneChange
    SceneChange

---
+++ Assets/SceneManagerScr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScr : MonoBehaviour {
+
+    public void SceneChange1() {
+        SceneManager.LoadScene(1);
+    }
+}
